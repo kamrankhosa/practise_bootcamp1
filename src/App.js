5a17d744363c5a75cf5276f3fcb8a2f8e53ca5b5
kamrankhosa <37815528+kamrankhosa@users.noreply.github.com>
2020-06-07 10:10:42 -0700

    updated div class to app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
     return ( 
-        <div>
+        <div class="App">
             <h1>Hello World</h1>
             <h1>From all of us!</h1>
             </div>
